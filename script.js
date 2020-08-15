c32648fc190df74934702374a8818e6d9594b926
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2020-08-15 05:41:18 -0400

    add all subpages for the website and incorporate a navigation menu on each subpage with proper anchors connecting each page together.

--- script.js
+++ script.js
@@ -69,3 +69,14 @@ $(function() {
 		}
 	}) ;
 });
+
+// Subpage Nav Menu Functions
+
+function openNav() {
+	document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+  }
